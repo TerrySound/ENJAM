76d6aa5c3ebf93c4c43aea725ad94f9f4db6dbc8
StarSlash <genma23@hotmail.fr>
2019-10-19 15:19:31 +0200

    [ADD]PlayerMovement Script [ADD]Script Testing Scene

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    [Range(1f, 100f)]
+    [Tooltip("The speed of the character")]
+    private float speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.movePlayer(Input.GetAxis("Horizontal"));
+    }
+
+    void movePlayer(float dir)
+    {
+        this.transform.position += new Vector3(dir, 0, 0)*Time.unscaledDeltaTime*this.speed;
+    }
+}
