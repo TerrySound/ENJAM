2fe1d1c4cdd59cd5ff72c7c4404497859e51c670
terrypg34 <56301490+terrypg34@users.noreply.github.com>
2019-10-19 18:58:20 +0200

    [ADD] Script SoundTrigger

--- Assets/Scripts/SoundTriggerAmbience.cs
+++ Assets/Scripts/SoundTriggerAmbience.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SoundTriggerAmbience : MonoBehaviour
-    
-{ 
-   
-    
-       
-
-
+{
+    public string EventNameEnter;
+    public string EventNameExit;
+    private void OnTriggerEnter(Collider Collider)
+    { 
+        AkSoundEngine.PostEvent(EventNameEnter, gameObject);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        AkSoundEngine.PostEvent(EventNameExit, gameObject);
+    }
 }
