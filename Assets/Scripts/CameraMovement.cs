e5c13fb484c463fbe8f2b5ad3f4b102c285ea2cc
heurteph <pierrehenri.heurte@gmail.com>
2019-10-20 11:27:08 +0200

    [ADD] CameraMovementScript / Test_Camera scene

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField]
+    [Tooltip("The game object to follow")]
+    public GameObject character;
+
+    private Vector3 originPosition = Vector3.zero;
+    private bool isSwitchingMode = false;
+    private float t = 0f;
+    private float speed = 0.8f;
+
+    private bool isScrolling = false;
+    private Vector3 lastPosition = Vector3.zero;
+    private bool isLeftLimit = false;
+    private bool isRightLimit = false;
+
+    [SerializeField]
+    [Tooltip("The slack of the camera")]
+    public float slack = 0.5f;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Return)) // Simulate trigger
+        {
+            originPosition = transform.position;
+            isSwitchingMode = true;
+        }
+
+        if (isSwitchingMode)
+        {
+            t = Mathf.Min(t + Time.deltaTime * speed, 1f); // t in [0,1]
+            float smoothstep = t * t * (3 - 2 * t);
+            transform.position = Vector3.Lerp(originPosition, character.transform.position + Vector3.forward * originPosition.z, smoothstep);
+
+            if (t == 1f)
+            {
+                isSwitchingMode = false;
+                isScrolling = true;
+            }
+        }
+
+        /* Slack of the camera */
+
+        if (isScrolling)
+        {
+            float distanceToCharacter = character.transform.position.x - transform.position.x;
+            float translationX = transform.position.x - lastPosition.x;
+            if (!isRightLimit && distanceToCharacter >= slack)
+            {
+                transform.parent = character.transform;
+                isRightLimit = true;
+            }
+            else if (!isLeftLimit && distanceToCharacter <= -slack)
+            {
+                transform.parent = character.transform;
+                isLeftLimit = true;
+            }
+            else if (isRightLimit && translationX < 0)
+            {
+                transform.parent = null;
+                isRightLimit = false;
+            }
+            else if (isLeftLimit && translationX > 0)
+            {
+                transform.parent = null;
+                isLeftLimit = false;
+            }
+        }
+
+        lastPosition = transform.position;
+    }
+}
