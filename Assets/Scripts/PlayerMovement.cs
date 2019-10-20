2f58306728d092fe1202980c7ed3fb0d7278d4f8
StarSlash <genma23@hotmail.fr>
2019-10-20 10:52:38 +0200

    [ADD] People Groups [UPDATE] Player Movement - now send the character X-position to Wwise

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,13 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[DisallowMultipleComponent]
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
     [Range(1f, 100f)]
     [Tooltip("The speed of the character")]
-    private float speed;
+    private float speed = 5;
 
+    void Awake()
+    {
+        this.GetComponent<Rigidbody2D>().gravityScale = 0;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +26,7 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         this.movePlayer(Input.GetAxis("Horizontal"));
+        AkSoundEngine.SetRTPCValue("RTPC_MC_Position", this.transform.position.x);
     }
 
     void movePlayer(float dir)
