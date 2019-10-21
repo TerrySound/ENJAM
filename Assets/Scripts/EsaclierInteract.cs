1388cd950121d1d7afd339b22ec7904ebba939e6
Loïc da Silva <loic.dasilva@gmail.com>
2019-10-22 01:45:24 +0200

    [UPDATE] UI pour les escaliers

--- Assets/Scripts/EsaclierInteract.cs
+++ Assets/Scripts/EsaclierInteract.cs
@@ -24,11 +24,15 @@ public class EsaclierInteract : PeopleGroup
     private void OnTriggerEnter2D(Collider2D other)
     {
         EventManager.OnInteract += SquallalaNousSommesPartis;
+        GameObject.Find("E Button Interact").GetComponent<SpriteRenderer>().enabled = true;
+        GameObject.Find("Interact").GetComponent<MeshRenderer>().enabled = true;
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
         EventManager.OnInteract -= SquallalaNousSommesPartis;
+        GameObject.Find("E Button Interact").GetComponent<SpriteRenderer>().enabled = false;
+        GameObject.Find("Interact").GetComponent<MeshRenderer>().enabled = false;
     }
 
     public void SquallalaNousSommesPartis()
