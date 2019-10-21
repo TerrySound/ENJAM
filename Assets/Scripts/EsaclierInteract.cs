9387df00707f89453bba3a505a68ea0689967587
StarSlash <genma23@hotmail.fr>
2019-10-21 17:06:51 +0200

    Merge branch 'master' of https://github.com/TerrySound/ENJAM

--- Assets/Scripts/EsaclierInteract.cs
+++ Assets/Scripts/EsaclierInteract.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EsaclierInteract : MonoBehaviour
+public class EsaclierInteract : PeopleGroup
 {
+    public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +15,21 @@ public class EsaclierInteract : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        EventManager.OnInteract += SquallalaNousSommesPartis;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        EventManager.OnInteract -= SquallalaNousSommesPartis;
+    }
+
+    public void SquallalaNousSommesPartis()
+    {
+        player.transform.position = new Vector3(8.3f, player.transform.position.y, player.transform.position.z);
+       
     }
 }
