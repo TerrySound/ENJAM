ffdab566c3cb321a93cc66a7716e8a3ebe87d7af
Loïc da Silva <loic.dasilva@gmail.com>
2019-10-23 00:17:14 +0200

    [UPDATE] Les persos se tournent sauf Kimberley kk

---
+++ Assets/Scripts/TalkingToMarcel.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalkingToMarcel : PeopleGroup
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        EventManager.OnInteract += Turn;
+        GameObject.Find("E Button Interact").GetComponent<SpriteRenderer>().enabled = true;
+        GameObject.Find("Interact").GetComponent<MeshRenderer>().enabled = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        EventManager.OnInteract -= Turn;
+        GameObject.Find("E Button Interact").GetComponent<SpriteRenderer>().enabled = false;
+        GameObject.Find("Interact").GetComponent<MeshRenderer>().enabled = false;
+    }
+
+    public void Turn()
+    {
+        this.GetComponent<Animator>().SetTrigger("talkToMarcel");
+    }
+
+
+}
